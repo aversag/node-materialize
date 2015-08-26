caddc849431d258127d84d81e5bbfc00aab71aa3
jvhoven <wolferine@live.se>
2015-08-26 14:34:55 +0200

    It now works, yay

---
+++ index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+(function(module) {
+  var Materialize = module.exports = {
+    inject: function() {
+      window.jQuery = require('./node_modules/jquery/dist/jquery.min.js');
+      window.$ = window.jQuery;
+      require('hammerjs');
+      require('./bin/materialize.js');
+    }
+  }
+})
