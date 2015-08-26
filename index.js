64720df188cae3ff4434afe5e20cee5e15279ecc
jvhoven <wolferine@live.se>
2015-08-26 14:45:33 +0200

    Minor change & version bump

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 'use strict';
 
-(function(module) {
-  var Materialize = module.exports = {
-    inject: function() {
-      window.jQuery = require('./node_modules/jquery/dist/jquery.min.js');
-      window.$ = window.jQuery;
-      require('hammerjs');
-      require('./bin/materialize.js');
-    }
+var Materialize = module.exports = {
+  inject: function() {
+    window.jQuery = require('./node_modules/jquery/dist/jquery.min.js');
+    window.$ = window.jQuery;
+    require('hammerjs');
+    require('./bin/materialize.js');
   }
-})
+}
